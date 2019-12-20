e92c10624f8c46a958efe4728ffb4c7875ad34ea
Rajitha Disanayaka <RajithaD@99x.lk>
2019-12-20 19:34:03 +0530

    Gatsby Link

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from 'gatsby';
+
+export default () => (
+<>
+    <h1>About me</h1>
+    <p>Test para</p>
+
+    <Link to="/">&larr; back to home</Link>
+</>
+);
